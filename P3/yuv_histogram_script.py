1ab53349c01a0ea104880768a346f7ad821cfaf0
Ericderulo13 <125275818+Ericderulo13@users.noreply.github.com>
2023-11-21 23:22:33 +0100

    Add files via upload

---
+++ P3/yuv_histogram_script.py
@@ -0,0 +1,12 @@
+#imports
+from PIL import Image
+import numpy as np
+import subprocess
+import  matplotlib.pyplot as plt
+import numpy as np
+
+class yuvi():
+    def yuv_histogram(self,video):
+        result = subprocess.run(["ffmpeg"], stdout=subprocess.PIPE, text=True)
+        result1 = subprocess.run([f'ffmpeg -i {video} -vf "split=2[a][b],[b]histogram,format=yuva444p[hh],[a][hh]overlay" -c:a copy yuv_histogram.mp4'], shell=True,stdout=subprocess.PIPE, text=True)
+
