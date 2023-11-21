52d3ccb18d4afd6fe4baed290e253a70988c0d4d
Ericderulo13 <125275818+Ericderulo13@users.noreply.github.com>
2023-11-21 23:09:33 +0100

    Add files via upload

---
+++ P3/subtiles_script.py
@@ -0,0 +1,11 @@
+from PIL import Image
+import numpy as np
+import subprocess
+import  matplotlib.pyplot as plt
+import numpy as np
+
+class subtitles():
+    # Exercice 4
+    def subtitles_performing(self,video, subtitles):
+        result = subprocess.run(["ffmpeg"], stdout=subprocess.PIPE, text=True)
+        result1 = subprocess.run([f'ffmpeg -i {video} -vf "subtitles=eric.srt" -c:a copy -scodec mov_text subtitles_video.mp4'], shell=True,stdout=subprocess.PIPE, text=True)
